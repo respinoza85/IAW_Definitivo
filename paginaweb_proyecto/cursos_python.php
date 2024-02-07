25310dc5ad9d15b805e78c602280468db7d62db0
respinoza85 <156797596+respinoza85@users.noreply.github.com>
2024-02-07 10:57:04 +0100

    Create cursos_python.php

---
+++ paginaweb_proyecto/cursos_python.php
@@ -0,0 +1,59 @@
+<?php
+// Iniciar la sesión
+session_start();
+
+// Verificar si la sesión 'usuario_id' está definida
+//if (!isset($_SESSION['usuario_id'])) {
+    //die("No se ha iniciado sesión.");
+//}
+
+// Conexión a la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "saturno";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Obtener el ID del cliente de la sesión
+$id_cliente = $_SESSION['usuario_id'];
+
+// Verificar el ID del cliente
+echo "ID del cliente: " . $id_cliente . "<br>";
+
+// Consulta para obtener los cursos del cliente
+$sql = "select * from cursos where Tipo_de_curso = 'Python'";
+//$sql = "SELECT c.Id, c.Nombre, c.Tipo_de_curso, c.Precio
+  //      FROM cursos c
+    //    INNER JOIN usuario u ON c.Id = u.Id
+      //  WHERE u.Id = '$id_cliente'";
+
+// Mostrar la consulta SQL
+//echo "Consulta SQL: " . $sql . "<br>";
+
+$result = $conn->query($sql);
+
+if ($result === false) {
+    die("Error en la consulta: " . $conn->error);
+}
+
+if ($result->num_rows > 0) {
+    // Mostrar los cursos del cliente
+    while($row = $result->fetch_assoc()) {
+        echo "Nombre del curso: " . $row["Nombre"]. "<br>";
+        echo "Tipo de curso: " . $row["Tipo_de_curso"]. "<br>";
+        echo "Precio: " . $row["Precio"]. "<br><br>";
+    }
+} else {
+    echo "El cliente no tiene cursos.";
+}
+
+// Cerrar el resultado y la conexión
+$result->close();
+$conn->close();
+?>

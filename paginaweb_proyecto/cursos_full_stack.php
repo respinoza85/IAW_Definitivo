baa3d6f939fcf32e8a938a5d79428845ebdf4dc3
respinoza85 <156797596+respinoza85@users.noreply.github.com>
2024-02-09 18:30:08 -0500

    Update cursos_full_stack.php

--- paginaweb_proyecto/cursos_full_stack.php
+++ paginaweb_proyecto/cursos_full_stack.php
@@ -1,10 +1,20 @@
-<?php
-// Iniciar la sesión
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Título de tu página</title>
+    <link rel="stylesheet" type="text/css" href="estilos_cursos.css"> <!-- Aquí es donde enlazas tu archivo CSS -->
+</head>
+<body>
+    <!-- Contenido de tu página aquí -->
+    <?php
+  // Iniciar la sesión
 session_start();
 
 // Verificar si la sesión 'usuario_id' está definida
 //if (!isset($_SESSION['usuario_id'])) {
-    //die("No se ha iniciado sesión.");
+  //  die("No se ha iniciado sesión.");
 //}
 
 // Conexión a la base de datos
@@ -23,46 +33,47 @@ if ($conn->connect_error) {
 // Obtener el ID del cliente de la sesión
 //$id_cliente = $_SESSION['usuario_id'];
 
-// Verificar el ID del cliente echo "ID del cliente: " . $id_cliente . "<br>";
+// Verificar el ID del cliente
+//echo "ID del cliente: " . $id_cliente . "<br>";
 
 // Consulta para obtener los cursos del cliente
-$sql = "select * from cursos where Tipo_de_curso = 'Full-Stack'";
+$sql = "SELECT * FROM cursos WHERE Tipo_de_curso = 'Full_Stack'";
 
 // Mostrar mensaje de bienvenida
 echo "<h1><marquee>¡Bienvenido! Aquí están los cursos que tienes:</marquee></h1><br><br>";
 
-//$sql = "SELECT c.Id, c.Nombre, c.Tipo_de_curso, c.Precio
-  //      FROM cursos c
-    //    INNER JOIN usuario u ON c.Id = u.Id
-      //  WHERE u.Id = '$id_cliente'";
-
-// Mostrar la consulta SQL
-//echo "Consulta SQL: " . $sql . "<br>";
 
 $result = $conn->query($sql);
 
 if ($result === false) {
     die("Error en la consulta: " . $conn->error);
 }
+?>
 
-if ($result->num_rows > 0) {
-    // Mostrar los cursos del cliente
-    while($row = $result->fetch_assoc()) {
-        echo "Nombre del curso: " . $row["Nombre"]. "<br>";
-        echo "Tipo de curso: " . $row["Tipo_de_curso"]. "<br>";
-        echo "Precio: " . $row["Precio"]. "<br><br>";
+<div style="font-size: 18px; font-family: Arial;">
+<?php
+    if ($result->num_rows > 0) {
+        // Mostrar los cursos del cliente
+        while($row = $result->fetch_assoc()) {
+            echo "<strong>Nombre del curso:</strong> " . $row["Nombre"]. "<br>";
+            echo "<strong>Tipo de curso:</strong> " . $row["Tipo_de_curso"]. "<br>";
+            echo "<strong>Precio:</strong> " . $row["Precio"]. "<br><br>";
+        }
+    } else {
+        echo "El cliente no tiene cursos.";
     }
-} else {
-    echo "El cliente no tiene cursos.";
-}
+?>
+</div>
 
+<?php
 // Cerrar el resultado y la conexión
 $result->close();
 $conn->close();
 ?>
-
+</body>
+</html>
 <html>
-
+      
 <input type="button" onclick="window.location='Udemy.html'" class="Redirect" value="Volver atras"/>
-  
+
 </html>
